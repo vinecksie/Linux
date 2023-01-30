2f6588ed5baae73ca38785d4e26031db83d2e96a
Vincent Eckert Sierota <94926406+vinecksie@users.noreply.github.com>
2023-01-30 18:58:04 +0000

    Add new challenge of today

--- linux_challenge_processes.sh
+++ linux_challenge_processes.sh
@@ -11,3 +11,31 @@ ps aux --sort=-%mem > processes.txt
 top
 P
 space
+
+#Challenge #4
+top -b -d 1 -n 3 > topprocesses.txt
+
+#Challenge #5
+sudo apt install htop
+htop
+
+#Challenge #6
+pgrep term
+kill 3393
+
+#Challenge #7
+kill -SIGINT $(pidof firefox)
+
+#Challenge #8
+gedit &
+pkill gedit
+
+#Challenge #9
+ls -lR /
+jobs -l | grep lR
+fg %1
+
+#Challenge #10
+sleep 100 &
+pgrep -l sleep
+nohup sleep 100 &
