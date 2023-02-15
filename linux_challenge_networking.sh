833b600c1cd753f9665e5f882a72e882bc8da002
Vincent Eckert Sierota <94926406+vinecksie@users.noreply.github.com>
2023-02-15 20:41:20 +0000

    Add new challenge of today

--- linux_challenge_networking.sh
+++ linux_challenge_networking.sh
@@ -16,3 +16,28 @@ sudo ifconfig enp0s3 192.168.0.113/24 up
 
 sudo ip a del 192.168.0.112/24 dev enp0s3
 ip a add 192.168.0.113/24 dev enp0s3
+
+#Challenge 4
+sudo ifconfig enp0s3:10 192.168.0.200/24 up
+
+#Challenge 5
+sudo ifconfig enp0s3 down
+ifconfig enp0s3 hw ether 44:44:44:44:44:44
+ifconfig enp0s3 up
+
+#Challenge 6
+ping ***.***.***.***
+ping ***.***.***.***
+
+#Challenge 7
+sudo apt install openssh-server openssh-client
+systemclt status ssh
+ssh ***@***.***.***.***
+
+#Challenge 8
+
+#Challenge 9
+
+#Challenge 10
+
+#Challenge 11
