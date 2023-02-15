4afc8fdbedbbbbc54b349854d8454bab4f193b14
Vincent Eckert Sierota <94926406+vinecksie@users.noreply.github.com>
2023-02-15 17:55:36 +0000

    Add new challenge of today

--- linux_challenge_networking.sh
+++ linux_challenge_networking.sh
@@ -12,3 +12,7 @@ ifconfig -a enp0s3
 ip link show dev enp0s3
 
 #Challenge 3
+sudo ifconfig enp0s3 192.168.0.113/24 up
+
+sudo ip a del 192.168.0.112/24 dev enp0s3
+ip a add 192.168.0.113/24 dev enp0s3
