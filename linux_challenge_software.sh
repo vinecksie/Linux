96c9562823c5e7dd0bb6bf10c3b57a307986d73c
Vincent Eckert Sierota <94926406+vinecksie@users.noreply.github.com>
2023-02-25 20:46:52 +0000

    Add new challenge of today

--- linux_challenge_software.sh
+++ linux_challenge_software.sh
@@ -1,2 +1,3 @@
 #Challenge1
 wget https://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb
+dpkg -i https://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb
