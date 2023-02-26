a176d7017428cb96ff491506d15d2ff4b49883fe
Vincent Eckert Sierota <94926406+vinecksie@users.noreply.github.com>
2023-02-26 12:04:35 +0000

    Add new challenge of today

--- linux_challenge_software.sh
+++ linux_challenge_software.sh
@@ -1,3 +1,4 @@
 #Challenge1
 wget https://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb
 dpkg -i https://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb
+dpkg-query -l | grep chrome | dpkg --purge 
