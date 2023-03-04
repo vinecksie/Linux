8a02fc36815b2d3f9d7fe14bccf0e180583c11e8
Vincent Eckert Sierota <94926406+vinecksie@users.noreply.github.com>
2023-03-04 21:54:01 +0000

    Add new challenge of today

--- linux_challenge_software.sh
+++ linux_challenge_software.sh
@@ -2,3 +2,4 @@
 wget https://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb
 dpkg -i https://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb
 dpkg-query -l | grep chrome | dpkg --purge 
+apt install /home/vbox/Downloads/google-earth-stable_current_amd64.deb
